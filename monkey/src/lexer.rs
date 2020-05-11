332d572caefbbe293a15bfc31bf37bedd7337aa2
rariyama <ariyama@sansan.com>
2020-05-12 03:18:35 +0900

    テスト通るところまで実施

--- monkey/src/lexer.rs
+++ monkey/src/lexer.rs
@@ -1,5 +1,6 @@
 use super::token::{Token, TokenKind};
 
+
 pub struct Lexer<'a>  {
     input:        &'a str,
     position:     usize, // inputに対する現在の位置
@@ -36,32 +37,32 @@ impl<'a>  Lexer<'a>  {
         }
     }
 
-    fn next_token(&mut self) -> Token {
+    pub fn next_token(&mut self) -> Token {
         let token;
         match self.ch {
             b'=' => {
-                token = Self::new_token(TokenKind::ASSIGN, Self.ch);
+                token = Self::new_token(TokenKind::ASSIGN, self.ch);
             }
             b';' => {
-                token = Self::new_token(TokenKind::SEMICOLON, Self.ch);
+                token = Self::new_token(TokenKind::SEMICOLON, self.ch);
             }
             b'(' => {
-                token = Self::new_token(TokenKind::LPAREN, Self.ch);
+                token = Self::new_token(TokenKind::LPAREN, self.ch);
             }
             b')' => {
-                token = Self::new_token(TokenKind::RPAREN, Self.ch);
+                token = Self::new_token(TokenKind::RPAREN, self.ch);
             }
             b',' => {
-                token = Self::new_token(TokenKind::COMMA, Self.ch);
+                token = Self::new_token(TokenKind::COMMA, self.ch);
             }
             b'+' => {
-                token = Self::new_token(TokenKind::PLUS, Self.ch);
+                token = Self::new_token(TokenKind::PLUS, self.ch);
             }
             b'{' => {
-                token = Self::new_token(TokenKind::LBRACE, Self.ch);
+                token = Self::new_token(TokenKind::LBRACE, self.ch);
             }
             b'}' => {
-                token = Self::new_token(TokenKind::RBRACE, Self.ch);
+                token = Self::new_token(TokenKind::RBRACE, self.ch);
             }
             0 => {
                 token = Token {
@@ -70,21 +71,20 @@ impl<'a>  Lexer<'a>  {
                 };
             }
             _   => {
-                token = Self::new_token(TokenKind::ILLEGAL, Self.ch);
+                token = Self::new_token(TokenKind::ILLEGAL, self.ch);
             }
         }
         self.read_char();
+        println!("{:?}", token);
         return token;
     }
 }
-
-
 
 // if cfg(test) is written, test code is compiled only when test runs
 #[cfg(test)]// test runs only when execute cargo run
 mod testing {
     use crate::lexer::Lexer;
-    use crate::token::{Token, TokenKind};
+    use crate::token::TokenKind;
     //    use lexer::Lexer;//if you need to refer the code inside this file, you can use 'super'
 //    use token::TokenKind;
 
@@ -105,10 +105,11 @@ mod testing {
 
 //    let mut l = Lexer::new(input.to_string());
     let mut lexer = Lexer::new(input);
-    for test in &tests {
-//        let token = lexer.next_token(); //ここで失敗する。
-//        assert_eq!(token.Type,  test.expectedToken);
-//        assert_eq!(token.Literal,  test.expectedLiteral);
+//    println!("{:?}", lexer.position);
+    for test in tests.iter() {
+        let _token = lexer.next_token();
+        assert_eq!(_token.Type,  test.0);
+        assert_eq!(_token.Literal, test.1);
         }
     }
 }
